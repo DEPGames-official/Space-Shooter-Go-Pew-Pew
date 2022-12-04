2010f61831670cac7d44fb688d3276523c2ed45c
TheToxicChemist01 <64322975+TheToxicChemist01@users.noreply.github.com>
2022-12-04 20:51:39 +0200

    Added basic health reduction using Raycast 2D

--- Space Shooter/Assets/Scripts/Player/PlayerMechanics.cs
+++ Space Shooter/Assets/Scripts/Player/PlayerMechanics.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerMechanics : MonoBehaviour
 {
     public float Health = 100;
+    public MeteorMechanics meteorScript;
 
     // Start is called before the first frame update
     void Start()
@@ -12,11 +14,36 @@ public class PlayerMechanics : MonoBehaviour
                
     }
 
-    public Vector3 offset= new Vector3(0, 5, 0);
+    public Vector3 offset;
+    public RaycastHit2D gun;
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        RaycastHit2D gun = Physics2D.Raycast(transform.position + offset, Vector2.up);
-        print(gun.collider);
+        gun = Physics2D.Raycast(transform.position + offset, Vector2.up);
+        ShootGun();
+    }
+
+    public Collider2D meteorCollider;
+    void ShootGun()
+    {
+        try
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                print(gun.collider);
+                if (gun.collider.tag == meteorCollider.tag)
+                {
+                    var scriptHealth = gun.collider.GetComponent<MeteorMechanics>();
+                    print("SHOT");
+                    scriptHealth.health -= 25;
+                    print(scriptHealth.health);
+                }
+                else
+                {
+                    print("not Meteor");
+                }
+            }
+        }
+        catch(System.NullReferenceException) { }
     }
 }
